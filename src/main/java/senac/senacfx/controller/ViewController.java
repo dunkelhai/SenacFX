6e5bad8800520361180be5cad7a3bd6b711d2930
Alysson Oliveira <3oalysson@gmail.com>
2022-09-18 00:55:30 -0300

    Ajustes

--- src/main/java/senac/senacfx/controller/ViewController.java
+++ src/main/java/senac/senacfx/controller/ViewController.java
@@ -1,15 +1,19 @@
 package senac.senacfx.controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import senac.senacfx.gui.util.Alerts;
+import senac.senacfx.gui.util.Constraints;
 
+import java.net.URL;
 import java.util.Locale;
+import java.util.ResourceBundle;
 
-public class ViewController {
+public class ViewController implements Initializable {
     @FXML
     private TextField txt1;
     @FXML
@@ -38,5 +42,15 @@ public class ViewController {
     @FXML
     public void onBtTestAction(){
         Alerts.showAlert("ERRO", "DEU RUIM", "VAI EXPLODIR", Alert.AlertType.ERROR);
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        Constraints.setTextFieldDouble(txt1);
+        Constraints.setTextFieldDouble(txt2);
+        Constraints.setTextFieldMaxLength(txt1, 5);
+        Constraints.setTextFieldMaxLength(txt2, 5);
+        Constraints.setTextFieldInteger(txt1);
+        Constraints.setTextFieldInteger(txt2);
     }
 }
