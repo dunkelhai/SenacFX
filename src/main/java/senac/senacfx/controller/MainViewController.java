5b83fb5afbdddbd83b5898526d7ddbef6d68694e
Alysson Oliveira <3oalysson@gmail.com>
2022-09-18 16:18:51 -0300

    ajustes dificeis

--- src/main/java/senac/senacfx/controller/MainViewController.java
+++ src/main/java/senac/senacfx/controller/MainViewController.java
@@ -3,9 +3,12 @@ package senac.senacfx.controller;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+
 import javafx.scene.layout.VBox;
 import senac.senacfx.application.Main;
 import senac.senacfx.gui.util.Alerts;
@@ -35,7 +38,7 @@ public class MainViewController implements Initializable {
 
     @FXML
     public void onMenuItemAboutAction(){
-        System.out.println("onMenuItemAboutAction");
+        loadView("/gui/About.fxml");
     }
 
     @Override
@@ -43,13 +46,19 @@ public class MainViewController implements Initializable {
 
     }
 
-    private void loadView(String absoluteName){
+    private synchronized void loadView(String absoluteName){
         try{
             FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
             VBox newVBox = loader.load();
 
             Scene mainScene = Main.getMainScene();
-            
+            VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
+
+            Node mainMenu = mainVBox.getChildren().get(0);
+            mainVBox.getChildren().clear();
+            mainVBox.getChildren().add(mainMenu);
+            mainVBox.getChildren().addAll(newVBox.getChildren());
+
         }catch (IOException e){
             Alerts.showAlert("IO EXCEPTION", "Error loading view", e.getMessage(), Alert.AlertType.ERROR);
         }
