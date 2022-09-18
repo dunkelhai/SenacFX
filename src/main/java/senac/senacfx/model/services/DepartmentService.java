e7021d810641495d78465c7f601fb1bd95e7c3da
Alysson Oliveira <3oalysson@gmail.com>
2022-09-18 18:34:44 -0300

    Update DepartmentService.java
    feito o acesso a dados

--- src/main/java/senac/senacfx/model/services/DepartmentService.java
+++ src/main/java/senac/senacfx/model/services/DepartmentService.java
@@ -1,5 +1,7 @@
 package senac.senacfx.model.services;
 
+import senac.senacfx.model.dao.DaoFactory;
+import senac.senacfx.model.dao.DepartmentDao;
 import senac.senacfx.model.entities.Department;
 
 import java.util.ArrayList;
@@ -7,12 +9,17 @@ import java.util.List;
 
 public class DepartmentService {
 
+    //dependencia injetada usando padrao factory
+    private DepartmentDao dao = DaoFactory.createDepartmentDao();
+
     public List<Department> findAll(){
+        return dao.findAll();
+
         //Dados MOCK (fake) so para testar, sem puxar do banco por hora
-        List<Department> list = new ArrayList<>();
-        list.add(new Department(1,"Computadores"));
-        list.add(new Department(2,"Alimentação"));
-        list.add(new Department(3,"Financeiro"));
-        return list;
+//        List<Department> list = new ArrayList<>();
+//        list.add(new Department(1,"Computadores"));
+//        list.add(new Department(2,"Alimentação"));
+//        list.add(new Department(3,"Financeiro"));
+//        return list;
     }
 }
