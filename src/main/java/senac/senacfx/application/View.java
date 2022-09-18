6631d3bfc7c2c9612ed0f78a30fe22132ad1e049
Alysson Oliveira <3oalysson@gmail.com>
2022-09-18 00:36:16 -0300

    nao funciona

--- src/main/java/senac/senacfx/application/View.java
+++ src/main/java/senac/senacfx/application/View.java
@@ -12,7 +12,7 @@ public class View extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(View.class.getResource("/gui/view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Teste1");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/senac/senacfx/controller/ViewController.java
+++ src/main/java/senac/senacfx/controller/ViewController.java
@@ -4,15 +4,29 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import senac.senacfx.gui.util.Alerts;
+
+import java.util.Locale;
 
 public class ViewController {
     @FXML
-    private Label welcomeText;
+    private TextField txt1;
+    @FXML
+    private TextField txt2;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Label labelResult;
+
+    @FXML
+    private Button btSum;
+    @FXML
+    public void onBtSumAction(){
+        Locale.setDefault(Locale.US);
+        double n1 = Double.parseDouble(txt1.getText());
+        double n2 = Double.parseDouble(txt2.getText());
+        double sum = n1 + n2;
+        labelResult.setText(String.format("%.2f", sum));
     }
 
     @FXML
