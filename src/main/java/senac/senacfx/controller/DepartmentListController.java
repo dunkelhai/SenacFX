2a2357a513117ef849891aa463679e8977c6141a
Alysson Oliveira <3oalysson@gmail.com>
2022-09-18 16:41:45 -0300

    Ajustes

--- src/main/java/senac/senacfx/controller/DepartmentListController.java
+++ src/main/java/senac/senacfx/controller/DepartmentListController.java
@@ -1,13 +1,49 @@
 package senac.senacfx.controller;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+import senac.senacfx.application.Main;
+import senac.senacfx.model.entities.Department;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class DepartmentListController implements Initializable {
+
+    @FXML
+    private TableView<Department> tableViewDepartment;
+
+    @FXML
+    private TableColumn<Department, Integer> tableColumnId;
+
+    @FXML
+    private TableColumn<Department, String> tableColumnName;
+
+    @FXML
+    private Button btNew;
+
+    @FXML
+    public void onBtNewAction(){
+        System.out.println("onBtNewAction");
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        
+        initializeNodes();
+
+    }
+
+    private void initializeNodes() {
+        tableColumnId.setCellValueFactory(new PropertyValueFactory<>("id"));
+        tableColumnName.setCellValueFactory(new PropertyValueFactory<>("name"));
+
+        Stage stage = (Stage) Main.getMainScene().getWindow();
+        tableViewDepartment.prefHeightProperty().bind(stage.heightProperty());
+
     }
 }
