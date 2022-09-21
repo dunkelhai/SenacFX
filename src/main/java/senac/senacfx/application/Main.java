c766b270cb0111bcd0659dd6fd6a197739eee7b2
Alysson Oliveira <3oalysson@gmail.com>
2022-09-21 01:38:21 -0300

    Ajustes

--- src/main/java/senac/senacfx/application/Main.java
+++ src/main/java/senac/senacfx/application/Main.java
@@ -24,7 +24,7 @@ public class Main extends Application {
 
             mainScene = new Scene(scrollPane);
             primaryStage.setScene(mainScene);
-            primaryStage.setTitle("Sample JavaFX application");
+            primaryStage.setTitle("Titulo do seu sistema");
             primaryStage.show();
         } catch (IOException e) {
             e.printStackTrace();
--- src/main/java/senac/senacfx/controller/DepartmentFormController.java
+++ src/main/java/senac/senacfx/controller/DepartmentFormController.java
@@ -1,4 +1,51 @@
 package senac.senacfx.controller;
 
-public class DepartmentFormController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import senac.senacfx.gui.util.Constraints;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class DepartmentFormController implements Initializable {
+
+    @FXML
+    private TextField txtId;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    private Label labelErrorName;
+
+    @FXML
+    private Button btSave;
+
+    @FXML
+    private Button btCancel;
+
+    @FXML
+    public void onBtSaveAction() {
+        System.out.println("onBtSaveAction");
+    }
+
+    @FXML
+    public void onBtCancelAction() {
+        System.out.println("onBtCancelAction");
+    }
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        initializeNodes();
+    }
+
+    private void initializeNodes() {
+        Constraints.setTextFieldInteger(txtId);
+        Constraints.setTextFieldMaxLength(txtName, 30);
+
+    }
 }
