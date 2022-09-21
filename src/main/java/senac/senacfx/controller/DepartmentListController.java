6378d4e1542e3d68426bdf8779e2ffac0b8b7f72
Alysson Oliveira <3oalysson@gmail.com>
2022-09-21 02:40:31 -0300

    funciona

--- src/main/java/senac/senacfx/controller/DepartmentListController.java
+++ src/main/java/senac/senacfx/controller/DepartmentListController.java
@@ -83,6 +83,7 @@ public class DepartmentListController implements Initializable, DataChangeListen
         List<Department> list = service.findAll();
         obsList = FXCollections.observableArrayList(list);
         tableViewDepartment.setItems(obsList);
+        initEditButtons();
     }
 
     private void createDialogForm(Department obj, String absoluteName, Stage parentStage){
