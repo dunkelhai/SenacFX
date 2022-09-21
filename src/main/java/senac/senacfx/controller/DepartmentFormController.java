a229e24fcb3f7a89f44657deee5951b6327b9836
Alysson Oliveira <3oalysson@gmail.com>
2022-09-21 01:48:46 -0300

    Update DepartmentFormController.java

--- src/main/java/senac/senacfx/controller/DepartmentFormController.java
+++ src/main/java/senac/senacfx/controller/DepartmentFormController.java
@@ -5,12 +5,16 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import senac.senacfx.db.DbException;
 import senac.senacfx.gui.util.Constraints;
+import senac.senacfx.model.entities.Department;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class DepartmentFormController implements Initializable {
+
+    private Department entity;
 
     @FXML
     private TextField txtId;
@@ -26,6 +30,11 @@ public class DepartmentFormController implements Initializable {
 
     @FXML
     private Button btCancel;
+
+    //Contolador agora tem uma instancia do departamento
+    private void setDepartment(Department entity){
+        this.entity = entity;
+    }
 
     @FXML
     public void onBtSaveAction() {
@@ -48,4 +57,14 @@ public class DepartmentFormController implements Initializable {
         Constraints.setTextFieldMaxLength(txtName, 30);
 
     }
+
+    private void updateFormData(){
+        if (entity == null){
+            throw new IllegalStateException("Entidade nula");
+        }
+        
+        txtId.setText(String.valueOf(entity.getId()));
+        txtName.setText(entity.getName());
+    }
+
 }
