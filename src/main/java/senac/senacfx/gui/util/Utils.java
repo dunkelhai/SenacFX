139d18c2b29376f050202a0e9302f5d14027588b
Alysson Oliveira <3oalysson@gmail.com>
2022-09-21 01:55:06 -0300

    Update Utils.java

--- src/main/java/senac/senacfx/gui/util/Utils.java
+++ src/main/java/senac/senacfx/gui/util/Utils.java
@@ -10,4 +10,13 @@ public class Utils {
         return (Stage) ((Node)event.getSource()).getScene().getWindow();
     }
 
+    public static Integer tryParseToInt(String str){
+        try {
+            return Integer.parseInt(str);
+        } catch (NumberFormatException e){
+            return null;
+        }
+
+    }
+
 }
