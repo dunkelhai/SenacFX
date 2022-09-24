fa93578d2ae840e059e5ddc665517eec40905c56
Alysson Oliveira <3oalysson@gmail.com>
2022-09-24 16:11:20 -0300

    Update SellerListController.java

--- src/main/java/senac/senacfx/controller/SellerListController.java
+++ src/main/java/senac/senacfx/controller/SellerListController.java
@@ -91,7 +91,7 @@ public class SellerListController implements Initializable, DataChangeListener {
         initRemoveButtons();
     }
 
-//    private void createDialogForm(Seller obj, String absoluteName, Stage parentStage){
+    private void createDialogForm(Seller obj, String absoluteName, Stage parentStage){
 //        try {
 //            FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
 //            Pane pane = loader.load();
@@ -113,7 +113,7 @@ public class SellerListController implements Initializable, DataChangeListener {
 //        } catch (IOException e){
 //            Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), Alert.AlertType.ERROR);
 //        }
-//    }
+    }
 
     @Override
     public void onDataChanged() {
