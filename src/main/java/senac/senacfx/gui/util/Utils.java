0bd26de4af7d6b189c5fdb6254f1c8c14e09be72
Alysson Oliveira <3oalysson@gmail.com>
2022-09-24 16:14:08 -0300

    Update Utils.java

--- src/main/java/senac/senacfx/gui/util/Utils.java
+++ src/main/java/senac/senacfx/gui/util/Utils.java
@@ -2,7 +2,13 @@ package senac.senacfx.gui.util;
 
 import javafx.event.ActionEvent;
 import javafx.scene.Node;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
 import javafx.stage.Stage;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class Utils {
 
@@ -16,7 +22,42 @@ public class Utils {
         } catch (NumberFormatException e){
             return null;
         }
+    }
 
+    public static <T> void formatTableColumnDate(TableColumn<T, Date> tableColumn, String format) {
+        tableColumn.setCellFactory(column -> {
+            TableCell<T, Date> cell = new TableCell<T, Date>() {
+                private SimpleDateFormat sdf = new SimpleDateFormat(format);
+                @Override
+                protected void updateItem(Date item, boolean empty) {
+                    super.updateItem(item, empty);
+                    if (empty) {
+                        setText(null);
+                    } else {
+                        setText(sdf.format(item));
+                    }
+                }
+            };
+            return cell;
+        });
+    }
+
+    public static <T> void formatTableColumnDouble(TableColumn<T, Double> tableColumn, int decimalPlaces) {
+        tableColumn.setCellFactory(column -> {
+            TableCell<T, Double> cell = new TableCell<T, Double>() {
+                @Override
+                protected void updateItem(Double item, boolean empty) {
+                    super.updateItem(item, empty);
+                    if (empty) {
+                        setText(null);
+                    } else {
+                        Locale.setDefault(Locale.US);
+                        setText(String.format("%."+decimalPlaces+"f", item));
+                    }
+                }
+            };
+            return cell;
+        });
     }
 
 }
